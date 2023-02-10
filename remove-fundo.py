ca83398bd5e89c36dbda70a5e63769a46c428552
Geisler Alves Dias <geisler.dias@fazenda.mg.gov.br>
2023-02-10 08:11:19 -0300

    criação de removedor de fundo.

---
+++ remove-fundo.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from rembg import remove
+from PIL import Image
+
+
+st.title('Removedor de fundo')
+arquivo = st.file_uploader('imagem a enviar',type=["png", "jpg", "jpeg"],accept_multiple_files=False)
+if arquivo is not None:
+    open('imagem.jpeg','wb').write(arquivo.read())
+    col1, col2 = st.columns([1,1])
+    col1.image(open('imagem.jpeg','rb').read())
+    open('imagem.png','wb').write(remove(open('imagem.jpeg','rb').read()))
+    # semfundo.save('imagem.png')
+    col2.image('imagem.png')
+    col2.download_button('Baixar imagem',open('imagem.png','rb').read(),arquivo.name.replace("jpeg","png").replace("jpg","png"))
+    # st.image(remove(imagem))
